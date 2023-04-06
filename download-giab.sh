2c3e2830cf4a93b8b6e67dd7ce1d6eae50e8a384
kevin3 <kevin.aitb@gmail.com>
2023-04-07 00:06:39 +0800

    Create download-giab.sh

---
+++ download-giab.sh
@@ -0,0 +1,5 @@
+time aws s3 sync s3://giab/data/AshkenazimTrio/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/HG002_HiSeq300x_fastq/140528_D00360_0018_AH8VC6ADXX/Project_RM8391_RM8392/Sample_2A1 . --no-sign-request
+
+
+time aws s3 cp s3://ilmn-dragen-giab-samples/WGS/precisionFDA_v2_HG002/HG002.novaseq.pcr-free.35x.R1.fastq.gz . --no-sign-request
+time aws s3 cp s3://ilmn-dragen-giab-samples/WGS/precisionFDA_v2_HG002/HG002.novaseq.pcr-free.35x.R2.fastq.gz . --no-sign-request
