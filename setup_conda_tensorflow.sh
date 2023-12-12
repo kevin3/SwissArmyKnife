8f46d39c7ad0c362771de28be812fec84413da5e
kevin3 <kevin.aitb@gmail.com>
2023-12-13 00:23:02 +0800

    Update setup_conda_tensorflow.sh

--- setup_conda_tensorflow.sh
+++ setup_conda_tensorflow.sh
@@ -3,8 +3,10 @@ conda update -n base -c defaults conda
 
 conda create -n tensorflow_cpu pip python=3.8
 activate tensorflow_cpu
-conda install tensorflow keras matplotlib seaborn scikit-learn
+conda install tensorflow keras matplotlib seaborn scikit-learn jupyter
+jupyter notebook
 
 conda create -n tensorflow_gpu pip python=3.8
 activate tensorflow_gpu
-conda install tensorflow-gpu keras-gpu matplotlib seaborn scikit-learn
+conda install tensorflow-gpu keras-gpu matplotlib seaborn scikit-learn jupyter
+jupyter notebook
